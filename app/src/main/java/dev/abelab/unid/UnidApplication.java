57641fb50b327fbd4389904a337aaf54a21a1c68
averak <abe12@mccc.jp>
2022-09-02 23:57:19 +0900

    [chore] gradle init

---
+++ app/src/main/java/dev/abelab/unid/UnidApplication.java
@@ -0,0 +1,16 @@
+package dev.abelab.unid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * UNID Application
+ */
+@SpringBootApplication
+public class UnidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnidApplication.class, args);
+    }
+
+}
